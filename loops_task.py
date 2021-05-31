141f4b6139df4e8a912a806c42d8fbed76a291db
BRL021\brl021.krk <dawid.pacia@brainly.com>
2021-05-31 13:04:36 +0200

    warmup

--- loops_task.py
+++ loops_task.py
@@ -1,6 +1,5 @@
 import time
 
-
 start_time = time.time()
 loops = 0
 while time.time() - start_time < 1:
@@ -8,8 +7,8 @@ while time.time() - start_time < 1:
 end_time = time.time()
 
 loops_for = 0
-for i in range(loops):
-    loops_for += 1
+for _ in range(loops):
+    a_var = 1
 
 for_time = time.time() - end_time
 print(for_time)
