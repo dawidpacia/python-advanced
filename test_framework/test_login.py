6abcbebfd113c6706c01dbecb969578726bd154b
BRL021\brl021.krk <dawid.pacia@brainly.com>
2021-06-07 14:41:43 +0200

    Dockerfile + assert_that

--- test_framework/test_login.py
+++ test_framework/test_login.py
@@ -1,4 +1,5 @@
 import requests
+from hamcrest import *
 
 URL = "https://reqres.in/api"
 LOGIN_URL = f"{URL}/login/"
@@ -8,19 +9,20 @@ STATUS_CODE_OK, STATUS_CODE_NOT_FOUND = 200, 400
 def test_login_valid():
     json_body = {"email": "eve.holt@reqres.in", "password": "cityslicka"}
     resp = requests.post(LOGIN_URL, data=json_body)
-    assert resp.status_code == STATUS_CODE_OK
-    assert "token" in resp.json()
+    assert_that(resp.status_code, equal_to(STATUS_CODE_OK))
+    assert_that(resp.json(), has_key("token"))
 
 
 def test_login_no_password():
     json_body = {"email": "test@test.com"}
     resp = requests.post(LOGIN_URL, data=json_body)
-    assert resp.status_code == STATUS_CODE_NOT_FOUND
-    assert resp.json()["error"] == "Missing password"
+    assert_that(resp.status_code, equal_to(STATUS_CODE_NOT_FOUND))
+    assert_that(resp.json()["error"], contains_string("Missing password"))
 
 
 def test_login_no_email():
     json_body = {}
     resp = requests.post(LOGIN_URL, data=json_body)
-    assert resp.status_code == STATUS_CODE_NOT_FOUND
-    assert resp.json()["error"] == "Missing email or username"
+    assert_that(resp.status_code, equal_to(STATUS_CODE_NOT_FOUND))
+    assert_that(resp.json()["error"], contains_string("email"))
+    assert_that(resp.json()["error"], contains_string("username"))
